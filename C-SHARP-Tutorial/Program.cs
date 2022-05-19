5792567070d1abe823b8200d4830429d8c73851b
meredith <meredithbrown302@gmail.com>
2022-05-19 19:24:08 -0400

    story w/ variables

--- C-SHARP-Tutorial/Program.cs
+++ C-SHARP-Tutorial/Program.cs
@@ -6,10 +6,16 @@ namespace C_SHARP_Tutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 25;
+
+            Console.WriteLine("There once was a man named " + characterName + ".");
+            Console.WriteLine("He was " + characterAge + " years old.");
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName + "...");
+            Console.WriteLine("...but didn't like being " + characterAge + ".");
+
             Console.ReadLine();
         }
     }
