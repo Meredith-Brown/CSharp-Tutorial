89a8499e896bd1c35f6ae15eb5260d4266eca77c
meredith <meredithbrown302@gmail.com>
2022-05-19 19:31:49 -0400

    data types

--- C-SHARP-Tutorial/Program.cs
+++ C-SHARP-Tutorial/Program.cs
@@ -6,17 +6,33 @@ namespace C_SHARP_Tutorial
     {
         static void Main(string[] args)
         {
-            string characterName = "Tom";
-            int characterAge;
-            characterAge = 25;
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            // float = least accurate
+            double gpa = 3.3; // in the middle
+            // decimal = most accurate
+            bool isMale = false;
 
-            Console.WriteLine("There once was a man named " + characterName + ".");
-            Console.WriteLine("He was " + characterAge + " years old.");
-            characterName = "Mike";
-            Console.WriteLine("He really liked the name " + characterName + "...");
-            Console.WriteLine("...but didn't like being " + characterAge + ".");
+            Console.WriteLine(30); // 30 is a constant
 
             Console.ReadLine();
         }
     }
 }
+
+//STORY W/ VARIABLES:
+
+//string characterName = "Tom";
+//int characterAge;
+//characterAge = 25;
+
+//Console.WriteLine("There once was a man named " + characterName + ".");
+//Console.WriteLine("He was " + characterAge + " years old.");
+//characterName = "Mike";
+//Console.WriteLine("He really liked the name " + characterName + "...");
+//Console.WriteLine("...but didn't like being " + characterAge + ".");
+
+//Console.ReadLine();
+
+//==================================================
