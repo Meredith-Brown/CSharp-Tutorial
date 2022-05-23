eec2e26869671dd4dd5cde6d903ba9badb7a1c9c
meredith <meredithbrown302@gmail.com>
2022-05-22 23:01:15 -0400

    get user input

--- C-SHARP-Tutorial/Program.cs
+++ C-SHARP-Tutorial/Program.cs
@@ -6,29 +6,11 @@ namespace C_SHARP_Tutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(5 + 8);
-            Console.WriteLine(5 - 8);
-            Console.WriteLine(5 / 8);
-            Console.WriteLine(5 * 8);
-            Console.WriteLine(5 % 2);
-            Console.WriteLine(4 + 2 * 3);
-            Console.WriteLine((4 + 2) * 3);
-            Console.WriteLine(5.0 + 8.1);
-            Console.WriteLine(5 + 8.1);
-            Console.WriteLine(5 / 2);
-            Console.WriteLine(5 / 2.0);
-            int num = 6;
-            Console.WriteLine(num);
-            num++;
-            Console.WriteLine(num);
-            Console.WriteLine(Math.Abs(-40));
-            Console.WriteLine(Math.Pow(3, 2));
-            Console.WriteLine(Math.Pow(3.8, 2));
-            Console.WriteLine(Math.Sqrt(36));
-            Console.WriteLine(Math.Max(4,90));
-            Console.WriteLine(Math.Min(4, 90));
-            Console.WriteLine(Math.Round(4.3));
-            Console.WriteLine(Math.Round(4.6));
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ". You are " + age + " years old.");
             Console.ReadLine();
         }
     }
@@ -74,3 +56,31 @@ namespace C_SHARP_Tutorial
 
 //==================================================
 
+//NUMBER BASICS:
+
+//Console.WriteLine(5 + 8);
+//Console.WriteLine(5 - 8);
+//Console.WriteLine(5 / 8);
+//Console.WriteLine(5 * 8);
+//Console.WriteLine(5 % 2);
+//Console.WriteLine(4 + 2 * 3);
+//Console.WriteLine((4 + 2) * 3);
+//Console.WriteLine(5.0 + 8.1);
+//Console.WriteLine(5 + 8.1);
+//Console.WriteLine(5 / 2);
+//Console.WriteLine(5 / 2.0);
+//int num = 6;
+//Console.WriteLine(num);
+//num++;
+//Console.WriteLine(num);
+//Console.WriteLine(Math.Abs(-40));
+//Console.WriteLine(Math.Pow(3, 2));
+//Console.WriteLine(Math.Pow(3.8, 2));
+//Console.WriteLine(Math.Sqrt(36));
+//Console.WriteLine(Math.Max(4, 90));
+//Console.WriteLine(Math.Min(4, 90));
+//Console.WriteLine(Math.Round(4.3));
+//Console.WriteLine(Math.Round(4.6));
+//Console.ReadLine();
+
+//==================================================
