641d1d697b65a0eea807f22c08548173492d41dc
meredith <meredithbrown302@gmail.com>
2022-05-22 22:21:26 -0400

    number basics

--- C-SHARP-Tutorial/Program.cs
+++ C-SHARP-Tutorial/Program.cs
@@ -6,8 +6,29 @@ namespace C_SHARP_Tutorial
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe Academy";
-            Console.WriteLine(phrase.Substring(8, 3));
+            Console.WriteLine(5 + 8);
+            Console.WriteLine(5 - 8);
+            Console.WriteLine(5 / 8);
+            Console.WriteLine(5 * 8);
+            Console.WriteLine(5 % 2);
+            Console.WriteLine(4 + 2 * 3);
+            Console.WriteLine((4 + 2) * 3);
+            Console.WriteLine(5.0 + 8.1);
+            Console.WriteLine(5 + 8.1);
+            Console.WriteLine(5 / 2);
+            Console.WriteLine(5 / 2.0);
+            int num = 6;
+            Console.WriteLine(num);
+            num++;
+            Console.WriteLine(num);
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Pow(3.8, 2));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(4,90));
+            Console.WriteLine(Math.Min(4, 90));
+            Console.WriteLine(Math.Round(4.3));
+            Console.WriteLine(Math.Round(4.6));
             Console.ReadLine();
         }
     }
@@ -45,5 +66,11 @@ namespace C_SHARP_Tutorial
 
 //==================================================
 
+//STRING BASICS:
 
+//string phrase = "Giraffe Academy";
+//Console.WriteLine(phrase.Substring(8, 3));
+//Console.ReadLine();
+
+//==================================================
 
